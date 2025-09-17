f5ea3fba694f85fe8c74e1a3abea79fca2d57e90
Anshuman <anshumangdg@gmail.com>
2025-09-17 07:32:40 +0000

    .

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -10,7 +10,8 @@ if (!admin.apps.length) {
       credential: admin.credential.cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: privateKey,
+        // Replace escaped newlines from the environment variable
+        privateKey: privateKey.replace(/\\n/g, '\n'),
       }),
     });
   } catch (error: any) {
