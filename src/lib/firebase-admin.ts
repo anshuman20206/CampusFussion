fb7c7c177ab3c023d50c3dbb1afccf4aba0491b4
Anshuman <anshumangdg@gmail.com>
2025-09-17 07:30:26 +0000

    .

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,11 +2,15 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+    if (!privateKey) {
+      throw new Error("FIREBASE_PRIVATE_KEY environment variable is not set.");
+    }
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: privateKey,
       }),
     });
   } catch (error: any) {
