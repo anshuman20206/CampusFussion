b44143b022e7fdfdf4ec8ce37ce4b77fcc9a020d
Anshuman <anshumangdg@gmail.com>
2025-11-13 22:17:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "campusconnect-1il6y",
+  "appId": "1:454378528376:web:b324c8d8a3ace2078cd78d",
+  "apiKey": "AIzaSyC4a4v80y0evAGtz_pCKGg-IV_e22JDo4U",
+  "authDomain": "campusconnect-1il6y.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "454378528376"
+};
